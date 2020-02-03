8a9846e2933174e81ed6f8c86cb29a826dbd401f
Kyle Smith <tikioph@gmail.com>
2020-02-02 19:06:35 -0800

    started adding services

---
+++ Happenin.Business/AutomapperProfileConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Happnin.Data;
+
+namespace BlogEngine.Business
+{
+    public class AutomapperProfileConfiguration : Profile
+    {
+        public AutomapperProfileConfiguration()
+        {
+            CreateMap<Event, Event>().ForMember(property => property.Id, option => option.Ignore());
+            CreateMap<User, User>().ForMember(property => property.Id, option => option.Ignore());
+            CreateMap<Location, Location>().ForMember(property => property.Id, option => option.Ignore());
+        }
+
+        static public IMapper CreateMapper()
+        {
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddMaps(Assembly.GetExecutingAssembly());
+            });
+
+            return mapperConfiguration.CreateMapper();
+        }
+    }
+}
