73d3e6840f8ea77b771cc8489853b89c5d1634d5
Caleb <caleb7hawks@gmail.com>
2020-02-09 09:20:46 -0800

    Added SubmitEvent

---
+++ Happnin/ClientApp/src/components/SubmitEvent.js
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+
+export class SubmitEvent extends Component {
+    static displayName = SubmitEvent.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Submit Event</h1>
+                <form>
+                    <label>Title: </label>
+                    <p><input type='text' placeholder='Event Title' name='name' /></p>
+                    <p><button>Send Message</button></p>
+                </form>
+
+            </div>
+
+        );
+    }
+}
