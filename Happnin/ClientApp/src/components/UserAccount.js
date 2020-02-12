f50c962cc4b9595cbe7099f3c7a3bea0497d3bf6
Jessica Holcomb <jholcomb1@eagles.ewu.edu>
2020-02-12 15:51:46 -0800

    some refactors and added event browsing

--- Happnin/ClientApp/src/components/UserAccount.js
+++ Happnin/ClientApp/src/components/UserAccount.js
@@ -16,32 +16,32 @@ export class UserAccount extends Component {
                     <div class="col align-self-start col-sm border rounded white-div">
                             <div class="row text-center">
                                 <img src="https://cdn4.iconfinder.com/data/icons/social-media-and-networking/480/02_social_medis_profile_female_placeholder_image_profile_female-512.png" class="img-fluid mt-0" alt="profile image"></img>
-                                <h3>Jane Doe </h3>
+                                <h3 class = 'header'>Jane Doe </h3>
                             </div>
                         </div>
                         <div class="col col-lg border rounded white-div">
-                            <h1>ABOUT</h1>
-                            <p>Username: </p>
+                            <h1 class = 'header'>ABOUT</h1>
+                            <p class = 'subHeader'>Username: </p>
                             <div>
-                                <p>First name: </p>
+                                <p class = 'subHeader'>First name: </p>
                             </div>
                             <div>
-                                <p>Last name: </p>
+                                <p class = 'subHeader'>Last name: </p>
                             </div>
                             <div>
-                                <p>City: </p>
+                                <p class = 'subHeader'>City: </p>
                             </div>
                             <div>
-                                <p>Birthday: </p>
+                                <p class = 'subHeader'>Birthday: </p>
                             </div>
                             <div>
-                                <p>Email: </p>
+                                <p class = 'subHeader'>Email: </p>
                             </div>
                             <div>
-                                <p>Phone: </p>
+                                <p class = 'subHeader'>Phone: </p>
                             </div>
                             <div>
-                                <p>Types of events I'm interested in: </p>
+                                <p class = 'subHeader'>Types of events I'm interested in: </p>
                             </div>
                         </div>
                 </div>
