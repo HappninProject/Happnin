3afc4094057cc9b586d09d9e406aa708a054275a
Hope Marshall <hopejmarshall@gmail.com>
2020-02-11 18:58:07 -0800

    Fixed input validation on User Sign Up page

--- Happnin/ClientApp/src/components/UserCreation.js
+++ Happnin/ClientApp/src/components/UserCreation.js
@@ -1,9 +1,12 @@
 ﻿import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './UserCreation.css';
-import { AvForm, AvField } from 'availity-reactstrap-validation';
 import { Button } from 'reactstrap';
 
+
+//Ask user if they're 13 or over, are you a robot verification through google api
+//check if zip code is valid
+//create dropdowns for user requirements
 export class UserCreation extends Component {
     constructor(props) {
         super(props);
@@ -13,53 +16,44 @@ export class UserCreation extends Component {
         return (
             <div id="accountform">
                 <h1>Create Account</h1>
-                <AvForm>
-                    <AvField name="fname" label="First name: " type="text" errorMessage="Invalid first name" validate={{
-                        required: { value: true },
-                        pattern: { value: '^[A-Za-z0-9]+$' },
-                        minLength: { value: 1 },
-                        maxLength: { value: 16 }
-                    }} />
-                    <AvField name="lname" label="Last name: " type="text" errorMessage="Invalid last name" validate={{
-                        required: { value: true },
-                        pattern: { value: '^[A-Za-z0-9]+$' },
-                        minLength: { value: 1 },
-                        maxLength: { value: 20 }
-                    }} />
-                    <AvField name="username" label="Username: " type="text" errorMessage="Invalid username" validate={{
-                        required: { value: true },
-                        pattern: { value: '^[A-Za-z0-9]+$' },
-                        minLength: { value: 5 },
-                        maxLength: { value: 20 }
-                    }} />
-                    <AvField name="email" label="Email: " type="email" errorMessage="Invalid email" validate={{
-                        required: { value: true },
-                        minLength: { value: 4 },
-                        maxLength: { value: 30 }
-                    }} />
-                    <AvField name="dob" label="Date of birth (MM/DD/YYYY): " type="text" errorMessage="Invalid date of birth" validate={{
-                        required: { value: true },
-                        pattern: { value: '(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d'}
-                    }} />
-                    <AvField name="phone" label="Phone number: " type="text" errorMessage="Invalid phone number" validate={{
-                        required: { value: true },
-                        pattern: { value: '^((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$'},
-                        minLength: { value: 8 },
-                        maxLength: { value: 13 }
-                    }} />
-                    <AvField name="zip" label="Zip code: " type="text" errorMessage="Invalid zip code" validate={{
-                        required: { value: true },
-                        /*pattern: { value: '^[A-Za-z0-9]+$' },*/
-                        minLength: { value: 5 },
-                        maxLength: { value: 20 }
-                    }} />
-                    <AvField name="password" label="Password (Must contain at least one uppercase and lowercase letter,
-                    a number, and a special character): " type="password" errorMessage="Invalid password" validate={{
-                        required: { value: true },
-                        pattern: { value: '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})'}
-                    }} />
+                <form>
+                    <label>
+                        First name: <br/>
+                        <input id="fname" name="fname" type="text" pattern="^[A-Za-z]+$" minLength="1" maxLength="30" required/>
+                    </label>
+                    <div>
+                        <label>
+                        Last name: <br/>
+                            <input id="lname" name="lname" type="text" pattern="^[A-Za-z]+$" minLength="1" maxLength="30" required/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                        Username: <br/>
+                            <input id="username" name="username" type="text" pattern="^[A-Za-z0-9]+$" minLength="4" maxLength="15" required/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                        Email: <br/>
+                            <input id="email" name="email" type="email" required/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                        Zip code (5 digit): <br/>
+                            <input id="zip" name="zip" type="text" pattern="\d{5}" maxLength="5" required/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                        Password: <br/>
+                            <input id="password" name="password" type="password" 
+                            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,25}$" required/>
+                        </label>
+                    </div>
                     <Button type="submit">Submit</Button>
-                </AvForm>
+                </form>
             </div>
         )
     }
