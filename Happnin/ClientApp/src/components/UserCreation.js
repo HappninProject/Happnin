70ed056bf77f651de3da01d9cf119728e71d562e
Salem Fenn <salemlfenn@gmail.com>
2020-02-22 16:58:31 -0800

    Refactored dropdown validation into React Code

--- Happnin/ClientApp/src/components/UserCreation.js
+++ Happnin/ClientApp/src/components/UserCreation.js
@@ -22,10 +22,23 @@ export class UserCreation extends Component {
             email: '',
         }, 
             loading: true,
-            show: false,
-            isValidZip: false   
+            showZip: false,
+            showUser: false,
+            showPassReq: false,
+            isValidZip: false,
+            isValidUserAlpha: false,
+            isValidUserMin: false,
+            isValidUserMax: true,
+            isValidPassLower: false,
+            isValidPassUpper: false,
+            isValidPassNum: false,
+            isValidPassMin: false,
+            isValidPassMax: true,
+            isValidPassSpecial: false
         }
         this.validateZip = this.validateZip.bind(this);
+        this.validateUsername = this.validateUsername.bind(this);
+        this.validatePassword = this.validatePassword.bind(this);
     }
 
     handleInputChange = (event) => {
@@ -42,220 +55,102 @@ export class UserCreation extends Component {
     }
 
     componentDidMount() {
-
-        $(function() {
-            //shows password requirements when user starts input
-            $("#password").focus(function() {
-                $('#passwordreq').slideDown();
-            });
-            $("#password").blur(function() {
-                $('#passwordreq').hide();
-            });
-
-            //shows username requirements when user starts input
-            $("#username").focus(function() {
-                $('#usernamereq').slideDown();
-            });
-            $("#username").blur(function() {
-                $('#usernamereq').hide();
-            });
-
-            //shows zip code requirements when user starts input
-/*            $("#zip").focus(function() {
-                $('#zipreq').slideDown();
-            });
-            $("#zip").blur(function() {
-                $('#zipreq').hide();
-            });*/
-
-            //checks while user is typing to see if username requirements are being met
-            $("#username").keyup(function() {
-                //gets username value
-                var usernameValue = $("#username").val();
-
-                //making sure username only contains alphanumeric characters
-                var regUsername = new RegExp("^[A-Za-z0-9]*$");
-                if (regUsername.test(usernameValue)) {
-                    //add valid class, get rid of invalid class
-                    $('#userAlphaNum').removeClass("invalid").addClass("valid");
-                }
-                else{
-                    $('#userAlphaNum').removeClass("valid").addClass("invalid");
-                }
-
-                if(usernameValue.length >= 4){
-                    $('#userMin').removeClass("invalid").addClass("valid");
-                }
-                else{
-                    $('#userMin').removeClass("valid").addClass("invalid");
-                }
-
-                if(usernameValue.length <= 15){
-                    $('#userMax').removeClass("invalid").addClass("valid");
-                }
-                else{
-                    $('#userMax').removeClass("valid").addClass("invalid");
-                }
-
-            });
-
-            // $("#zip").keyup(function(){
-            //     //gets zip code value
-            //     var zipValue = $("#zip").val();
-
-            //     //making sure zip code is 5 digits
-            //     if (zipValue.length == 5) {
-            //         //add valid class, get rid of invalid class
-            //         $('#zipDigits').removeClass("invalid").addClass("valid");
-            //     }
-            //     else{
-            //         $('#zipDigits').removeClass("valid").addClass("invalid");
-                    
-            //     }
-            // });
-
-            /*checks while user is typing to see if password requirements are being met, and changes font from red to green and
-            adds a checkmark if requirements are met*/
-            $("#password").keyup(function() {
-                //get password value
-                var value = $("#password").val();
-
-                //testing that password has at least one lowercase letter
-                var regLower = new RegExp("[a-z]");
-                if (regLower.test(value)) {
-                    $('#lower').removeClass("invalid").addClass("valid");
-                }
-                else{
-                    $('#lower').removeClass("valid").addClass("invalid");
-                }
-
-                //testing that password has at least one uppercase letter
-                var regUpper = new RegExp("[A-Z]");
-                if (regUpper.test(value)) {
-                    $('#upper').removeClass("invalid").addClass("valid");
-                }
-                else{
-                    $('#upper').removeClass("valid").addClass("invalid");
-                }
-
-                //testing that password has at least one number
-                var regNum = new RegExp("[0-9]");
-                if (regNum.test(value)) {
-                    $('#number').removeClass("invalid").addClass("valid");
-                }
-                else{
-                    $('#number').removeClass("valid").addClass("invalid");
-                }
-
-                //testing that password is at least 8 characters
-                if(value.length >= 8){
-                    $('#minlength').removeClass("invalid").addClass("valid");
-                }
-                else{
-                    $('#minlength').removeClass("valid").addClass("invalid");
-                }
-
-                //testing that password isn't longer than 30 characters
-                if(value.length > 30){
-                    $('#maxlength').removeClass("valid").addClass("invalid");
-                }
-                else{
-                    $('#maxlength').removeClass("invalid").addClass("valid");
-                }
-
-                //testing that password contains at least one special character
-                var regSpecial = new RegExp("[!@#$%^&*(),.?\":{}|<>]");
-                if (regSpecial.test(value)) {
-                    $('#special').removeClass("invalid").addClass("valid");
-                }
-                else{
-                    $('#special').removeClass("valid").addClass("invalid");
-                }
-            });
-        });
+        
     }
 
+    //these show or hide dropdowns if input is clicked on
+    showOrHideZip = () => this.setState((currentState) => ({showZip: !currentState.showZip}));
+    showOrHideUser = () => this.setState((currentState) => ({showUser: !currentState.showUser}));
+    showOrHidePassReq = () => this.setState((currentState) => ({showPassReq: !currentState.showPassReq}));
 
-    //this shows or hides the dropdowns that are focused on
-    showOrHide = () => this.setState((currentState) => ({show: !currentState.show}));
-
+    //this makes sure that the zip code is valid, then add the valid or invalid classes accordingly
     validateZip(event){
         var zipValue = $("#zip").val();
-        if(zipValue.length == 5){
-            this.setState({isValidZip: true});
-            console.log("Zip is valid");
-        }
-        else{
-            this.setState({isValidZip: false});
-            console.log("Zip is not valid");
-        }
+        var zipValid = document.getElementById('zipDigits');
+        zipValue.length == 5 ? this.setState({isValidZip: true}) : this.setState({isValidZip: false});
     }
 
+    //this makes sure the username is valid, then add the valid or invalid classes accordingly
+    validateUsername(event){
+        var usernameValue = $("#username").val();
+        var regAlpha = new RegExp("^[A-Za-z0-9]*$");
+        regAlpha.test(usernameValue) && usernameValue.length > 0? this.setState({isValidUserAlpha: true}) : this.setState({isValidUserAlpha: false});
+        usernameValue.length >= 4 ? this.setState({isValidUserMin: true}) : this.setState({isValidUserMin: false});
+        usernameValue.length <= 15 ? this.setState({isValidUserMax: true}) : this.setState({isValidUserMax: false});
+    }
 
-    
+    //this makes sure the password is valid, then add the valid or invalid classes accordingly
+    validatePassword(event){
+        var passValue = $("#password").val();
+        var regLower = new RegExp("[a-z]");
+        regLower.test(passValue) ? this.setState({isValidPassLower: true}) : this.setState({isValidPassLower: false});
+        var regUpper = new RegExp("[A-Z]");
+        regUpper.test(passValue) ? this.setState({isValidPassUpper: true}) : this.setState({isValidPassUpper: false});
+        var regNum = new RegExp("[0-9]");
+        regNum.test(passValue) ? this.setState({isValidPassNum: true}) : this.setState({isValidPassNum: false});
+        passValue.length >= 8 ? this.setState({isValidPassMin: true}) : this.setState({isValidPassMin: false});
+        passValue.length <= 30 ? this.setState({isValidPassMax: true}) : this.setState({isValidPassMax: false});
+        var regSpecial = new RegExp("[!@#$%^&*(),.?\":{}|<>]");
+        regSpecial.test(passValue) ? this.setState({isValidPassSpecial: true}) : this.setState({isValidPassSpecial: false});
+    }
 
     render() {
-        const focus = this.state.focus;
-        let zipDiv;
-
-        if (focus) {
-            
-        }
-
         return (
             <div id="accountform">
-                <h1 class="header">Sign Up!</h1>
+                <h1 className="header">Sign Up!</h1>
                 <form>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label>First name:</label>
-                        <input id="fname" class="form-control" name="firstName" type="text" pattern="^[A-Za-z]+$" 
+                        <input id="fname" className="form-control" name="firstName" type="text" pattern="^[A-Za-z]+$" 
                             minLength="1" maxLength="40" placeholder="Jane" 
                             value={this.state.user.firstName} onChange={this.handleInputChange} required/>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label>Last name:</label>
-                        <input id="lname" class="form-control" name="lastName" type="text" pattern="^[A-Za-z]+$" minLength="1" maxLength="40" placeholder="Doe"
-                            value={this.state.user.lastName} onChange={this.handleInputChange} required/>
+                        <input id="lname" className="form-control" name="lastName" type="text" pattern="^[A-Za-z]+$" minLength="1" maxLength="40" 
+                        placeholder="Doe" value={this.state.user.lastName} onChange={this.handleInputChange} required/>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label>Username:</label>
-                        <input id="username" class="form-control" name="userName" type="text" pattern="^[A-Za-z0-9]{4,15}$" placeholder="user123" 
-                            value={this.state.user.firstName} onChange={this.handleInputChange} required/>
+                        <input id="username" className="form-control" name="userName" type="text" pattern="^[A-Za-z0-9]{4,15}$" placeholder="user123" 
+                            value={this.state.user.firstName} onChange={this.handleInputChange} onFocus={this.showOrHideUser} 
+                            onBlur={this.showOrHideUser} onKeyUp={this.validateUsername} required/>
+                            {this.state.showUser &&
+                                <p id="userAlphaNum" className={this.state.isValidUserAlpha ? "valid":"invalid"}>Username must only contain
+                                 letters and numbers</p>}
+                            {this.state.showUser && <p id="userMin"  className={this.state.isValidUserMin ? "valid":"invalid"}>
+                                Username must be have a minimum length of 4</p>}
+                            {this.state.showUser && <p id="userMax" className={this.state.isValidUserMax ? "valid":"invalid"}>
+                                User must have a maximum length of 15</p>}
                     </div>
-                    <div id="usernamereq">
-                        <p id="userAlphaNum" className="valid">Username must only contain letters and numbers</p>
-                        <p id="userMin" className="invalid">Username must be have a minimum length of 4</p>
-                        <p id="userMax" className="valid">User must have a maximum length of 15</p>
-                    </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label>Email:</label>
-                        <input id="email" class="form-control" name="email" type="email" placeholder="example@gmail.com"
+                        <input id="email" className="form-control" name="email" type="email" placeholder="example@gmail.com"
                             value={this.state.user.email} onChange={this.handleInputChange} required/>
                     </div>
                     <div className="form-group">
                     <label>Zip code: </label>
-                        <input id="zip" className="rounded form-control" name="zipcode" type="number" pattern="^\d{5}$" placeholder="99004" onFocus={this.showOrHide} onBlur={this.showOrHide}
-                         onKeyUp={this.validateZip} required/>
-                        {this.state.show && <p id="zipDigits" className="invalid">Zip code must be 5 digits</p>}
+                        <input id="zip" className="form-control rounded" name="zipcode" type="number" pattern="^\d{5}$" placeholder="99004" 
+                        onFocus={this.showOrHideZip} onBlur={this.showOrHideZip} onKeyUp={this.validateZip} required/>
+                        {this.state.showZip && <p id="zipDigits" className={this.state.isValidZip ? "valid":"invalid"}>
+                            Zip code must be 5 digits</p>}
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label>Password:</label>
-                        <input id="password" class="form-control" name="password" type="password" 
+                        <input id="password" className="form-control" name="password" type="password" 
                         pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$" 
-                        value={this.state.user.password} onChange={this.handleInputChange} required/>
-                    </div>
-                    <div id="passwordreq">
-                    <h4>Password must contain the following:</h4>
-                        <p id="lower" className="invalid">At least one lowercase letter</p>
-                        <p id="upper" className="invalid">At least one uppercase letter</p>
-                        <p id="number" className="invalid">A number</p>
-                        <p id="special" className="invalid">At least one special character</p>
-                        <p id="minlength" className="invalid">Minimum 8 characters</p>
-                        <p id="maxlength" className="valid">Maximum 30 characters</p>
+                        value={this.state.user.password} onChange={this.handleInputChange} onFocus={this.showOrHidePassReq}
+                         onBlur={this.showOrHidePassReq} onKeyUp={this.validatePassword} required/>
+                         {this.state.showPassReq && <p id="lower" className={this.state.isValidPassLower ? "valid":"invalid"}>At least one lowercase letter</p>}
+                         {this.state.showPassReq && <p id="upper" className={this.state.isValidPassUpper ? "valid":"invalid"}>At least one uppercase letter</p>}
+                         {this.state.showPassReq && <p id="number" className={this.state.isValidPassNum ? "valid":"invalid"}>A number</p>}
+                         {this.state.showPassReq && <p id="special" className={this.state.isValidPassSpecial ? "valid":"invalid"}>At least one special character</p>}
+                         {this.state.showPassReq && <p id="minlength" className={this.state.isValidPassMin ? "valid":"invalid"}>Minimum 8 characters</p>}
+                         {this.state.showPassReq && <p id="maxlength" className={this.state.isValidPassMax ? "valid":"invalid"}>Maximum 30 characters</p>}
                     </div>
                     <div>
                     <input type="checkbox" name="13orolder" required/>
-                    <label for="13orolder"> I am 13 or older</label>
+                    <label htmlFor="13orolder"> I am 13 or older</label>
                     </div>
 
                     <Recaptcha
