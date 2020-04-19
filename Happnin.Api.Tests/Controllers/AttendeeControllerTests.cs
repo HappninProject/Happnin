4e841f51f05498bc70160da5537415e71cd07461
Kyle Smith <tikioph@gmail.com>
2020-04-18 21:16:12 -0700

    set up the tests for the attendee controller

---
+++ Happnin.Api.Tests/Controllers/AttendeeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Happnin.Business.Dto;
+using Happnin.Business.Services;
+using Happnin.Controllers;
+
+namespace Happnin.Tests.Controllers
+{
+    public class AttendeeControllerTests : BaseControllerTests<IAttendeeService, Attending, AttendingInput>
+    {
+        public override Attending CreateDto()
+        {
+            return new Attending
+            {
+                EventId =  new Random().Next(),
+                UserId = new Guid().ToString()
+            };
+        }
+
+        public override BaseController<Attending, AttendingInput> GetController(IAttendeeService service)
+        {
+            return new AttendeeController(service);
+        }
+    }
+}
