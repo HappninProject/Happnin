0b3ab80df6cda1939806e3fcf68320d17c3f503e
Kyle Smith <tikioph@gmail.com>
2020-04-18 21:18:56 -0700

    removed usings that were not needed

--- Happnin/Controllers/LocationController.cs
+++ Happnin/Controllers/LocationController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Happnin.Business.Dto;
+﻿using Happnin.Business.Dto;
 using Happnin.Business.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Happnin.Controllers
