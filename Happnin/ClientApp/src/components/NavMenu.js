caea95434407a834ce3b0d4668691dd4e9884d6b
Salem Fenn <salemlfenn@gmail.com>
2020-04-26 20:22:58 -0700

    Fixed NavMenu spacing, added route for Messsages and Friends pages

--- Happnin/ClientApp/src/components/NavMenu.js
+++ Happnin/ClientApp/src/components/NavMenu.js
@@ -12,27 +12,26 @@ import { LoginMenu } from "./api-authorization/LoginMenu";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/NavMenu.css";
-import Weather from './Weather';
+import Weather from "./Weather";
 import logo from "../images/happninHLogoCircle.png";
 import notification from "../images/bell.svg";
 import friends from "../images/users.svg";
 import messages from "../images/inbox.svg";
-import Dropdown from 'react-bootstrap/Dropdown'
+import Dropdown from "react-bootstrap/Dropdown";
 export class NavMenu extends Component {
-  
-    static displayName = NavMenu.name;
-    constructor(props) {
-        super(props);
-        this.toggleNavbar = this.toggleNavbar.bind(this);
-        this.state = {
-          collapsed: true,
-          //loading: true,
-        };
-        /*this.getLocation = this.getLocation.bind(this);
+  static displayName = NavMenu.name;
+  constructor(props) {
+    super(props);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true,
+      //loading: true,
+    };
+    /*this.getLocation = this.getLocation.bind(this);
         this.getCordinates = this.getCordinates.bind(this);
         this.getLocation();*/
-    }
-   /* getLocation() {
+  }
+  /* getLocation() {
       if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(this.getCordinates);
       }
@@ -45,24 +44,23 @@ export class NavMenu extends Component {
       })
     }*/
 
-    toggleNavbar() {
+  toggleNavbar() {
     this.setState({
       collapsed: !this.state.collapsed,
     });
-    }
+  }
 
   render() {
     return (
       <header className="container-fluid rounded shadow-sm">
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm mb-5" light>
+        <Navbar
+          id="nav-container"
+          className="navbar-expand-sm navbar-toggleable-sm mb-5"
+          light
+        >
           <Container className="navbar">
             <NavbarBrand tag={Link} to="/" className="logo">
-              <img
-                id="logo"
-                class="d-inline-block mr-1"
-                alt="Logo"
-                src={logo}
-              />
+              <img id="logo" class="d-inline-block" alt="Logo" src={logo} />
             </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse
@@ -104,16 +102,15 @@ export class NavMenu extends Component {
                     Events
                   </NavLink>
                 </NavItem>
-                <LoginMenu></LoginMenu>
+                <LoginMenu></LoginMenu>&nbsp;&nbsp;
                 <NavbarBrand tag={Link} to="/friends" className="friends">
                   <img
                     id="friends"
-                    class="d-inline-block mr-1"
+                    class="d-inline-block mr-3"
                     alt="friends"
                     src={friends}
                   />
                 </NavbarBrand>
-
                 <NavbarBrand tag={Link} to="/messages" className="messages">
                   <img
                     id="messages"
@@ -122,28 +119,31 @@ export class NavMenu extends Component {
                     src={messages}
                   />
                 </NavbarBrand>
-
-                
                 <Dropdown>
-                  <Dropdown.Toggle variant="link" size ="sm" id="dropdown-basic"  background-color = "b1ed82">
-                  <img
-                    id="notification"
-                    class="d-inline-block mr-1"
-                    alt="notifications"
-                    src={notification}
-                  />
+                  <Dropdown.Toggle
+                    variant="link"
+                    size="sm"
+                    id="dropdown-basic"
+                    background-color="b1ed82"
+                  >
+                    <img
+                      id="notification"
+                      class="d-inline-block"
+                      alt="notifications"
+                      src={notification}
+                    />
                   </Dropdown.Toggle>
-
                   <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1"> Woah its a notification</Dropdown.Item>
+                    <Dropdown.Item href="#/action-1">
+                      {" "}
+                      Woah its a notification
+                    </Dropdown.Item>
                   </Dropdown.Menu>
-                  
                 </Dropdown>
               </ul>
             </Collapse>
           </Container>
-          <Weather id = "weather"></Weather>
-          
+          <Weather id="weather"></Weather>
         </Navbar>
       </header>
     );
--- Happnin/ClientApp/src/components/friends.js
+++ Happnin/ClientApp/src/components/friends.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
-export class About extends Component {
+export class Friends extends Component {
   render() {
     return (
       <div className="container-fluid card">
         <div className="header"> Friends</div>
         <div className="subheader">
-            this is where a list of your friends will go
+          <p>This is where a list of your friends will go</p>
         </div>
       </div>
     );
