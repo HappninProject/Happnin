29fc4f24c900db035882f714c007e6ff3523d29e
Kyle Smith <tikioph@gmail.com>
2020-05-09 19:05:46 -0700

    have to get the events to render

--- Happnin/ClientApp/src/components/Event/HostedEvents.js
+++ Happnin/ClientApp/src/components/Event/HostedEvents.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+<<<<<<< refs/remotes/HappninProject/master
 import { HostEvent } from "./HostEvent";
+=======
+import { HappninEvent } from "./HappninEvent";
+>>>>>>> have to get the events to render
 import authService from '../api-authorization/AuthorizeService';
 
 export class HostedEvents extends Component {
@@ -12,11 +16,15 @@ export class HostedEvents extends Component {
             isAuthenticated: false,
             user: '',
             Events: [],
+<<<<<<< refs/remotes/HappninProject/master
             Locations: [], 
+=======
+>>>>>>> have to get the events to render
             loading: true
         }
     }
 
+<<<<<<< refs/remotes/HappninProject/master
     //TODO need to pull all the event information, like the location and such
 
   async componentDidMount(event) {
@@ -32,6 +40,20 @@ export class HostedEvents extends Component {
         {events.map((eventinfo) => (
           <HostEvent key={eventinfo.id} {...eventinfo} 
           location = {this.GetRightLocation(eventinfo.locationId, location)} 
+=======
+  async componentDidMount(event) {
+    this._subscription = authService.subscribe(() => this.populateState());
+    await this.populateState(); 
+    this.GetHostedEvents(); 
+  };
+
+  static renderEventsTable(events, userId) {
+    return (
+      <div>
+        {events.map((eventinfo) => (
+          <HappninEvent key={eventinfo.id} {...eventinfo} 
+          attending={eventinfo.going}
+>>>>>>> have to get the events to render
           userId={userId}
           />
         ))}
@@ -52,6 +74,7 @@ export class HostedEvents extends Component {
   }
 
   render() {
+<<<<<<< refs/remotes/HappninProject/master
    let contents = this.state.loading ? (
       <p>
         <em>Loading...</em>
@@ -69,6 +92,14 @@ export class HostedEvents extends Component {
         </div>
       </div>
     );
+=======
+    if(this.loading){
+        return (
+            <div><h1>Here in my hosted events</h1></div>         
+        ) 
+    }
+    return HostedEvents.renderEventsTable(this.state.event, this.state.userId);
+>>>>>>> have to get the events to render
  }
 
   async GetHostedEvents(){
@@ -76,8 +107,11 @@ export class HostedEvents extends Component {
     console.log("user id" + userId)
     const response = await fetch(`api/Event/HostedEvent/${userId}`);
     const data = await response.json();
+<<<<<<< refs/remotes/HappninProject/master
     console.log("Data");
     console.log(data);
+=======
+>>>>>>> have to get the events to render
     this.setState({ Events: data, loading: false });
   }
 
