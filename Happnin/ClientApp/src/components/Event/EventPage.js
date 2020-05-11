411cca424c2a323a9af1ec849ad414bbbb9964cb
Jessica Holcomb <jholcomb1@eagles.ewu.edu>
2020-05-11 12:52:03 -0700

    Auto stash before rebase of "HappninProject/master"

--- Happnin/ClientApp/src/components/Event/EventPage.js
+++ Happnin/ClientApp/src/components/Event/EventPage.js
@@ -1,19 +1,45 @@
 ﻿import React, { Component } from "react";
 import image from "../../images/event-image.jpg";
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
 export class EventPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          lat: 0, 
+          lng: 0, 
+          zoom: 13,
+          loading: true, 
+        };
+    }
+    async componentDidMount() {
+
+    }
     render() {
         return (
+            
             <div className="card container-fluid">
-                <div className="col-3 border rounded white-div">
+                <div id="event-image-container">
                         <img
                             id="event-image"
                             alt="event image"
                             src={image}
                         />
+                    </div>
+                    <h1 className="header">Event Name</h1>
+                <div id = "map-container">
+                    <Map 
+                        center={[this.state.lat, this.state.lng]} 
+                        zoom={this.state.zoom} 
+                        style={{ width: '75vh', height: '75vh', float:"right"}}
+                        >
+                        <TileLayer
+                            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                            url="https://{s}-tiles.locationiq.com/v2/obk-en/r/{z}/{x}/{y}.png?key=b0b149aa2f9d3a"
+                        />
+                    </Map>
                 </div>
-                <h1 className="header">Event Name</h1>
-             </div>
+            </div>
 
             );
     }
